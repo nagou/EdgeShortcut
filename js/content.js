08f849eb21e0bd6bf214abc23e64c89775a3ef16
Trishu Dey <trdey@microsoft.com>
2018-07-23 17:02:07 +0530

    super-script changes

--- js/content.js
+++ js/content.js
@@ -10,14 +10,6 @@ while (len--){
     var a = anchors[len];
     anchor_nodes.push(a); // push the node object in case that needs to change
     url_pages.push(a.href); // push the href attribute to the array of hrefs
-    if (!isHidden(a))
-        alert(a.text);
-    a.innerHTML = a.text+" " + "<b> <font color = 'red'>" + len + "</font></b>";
 
-    alert(a.text);
-}
-
-function isHidden(el) {
-    var style = window.getComputedStyle(el);
-    return ((style.display === 'none') || (style.visibility === 'hidden'))
+    a.innerHTML = "<sup><b> <font color = 'red'>" + len + "</font></b></sup>" + a.text;
 }
