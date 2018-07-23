26477dbfe2d3318335366f44317ced6a4727a5aa
Trishu Dey <trdey@microsoft.com>
2018-07-23 19:15:03 +0530

    image fix

--- js/content.js
+++ js/content.js
@@ -5,11 +5,21 @@ var url_pages = [], anchor_nodes = [];
 
 var anchors = document.links
 var len = anchors.length
-alert(len)
+
 while (len--){
     var a = anchors[len];
     anchor_nodes.push(a); // push the node object in case that needs to change
     url_pages.push(a.href); // push the href attribute to the array of hrefs
 
-    a.innerHTML = "<sup><b> <font color = 'red'>" + len + "</font></b></sup>" + a.text;
+    var n = a.innerHTML.indexOf("img")
+    var s = a.innerHTML.indexOf("span")
+    //alert(n +" " +a.innerHTML)
+    console.log(a.innerHTML+","+n + " " + len)
+    console.log(n == -1);
+     if( n+"" == "-1" && s+"" == "-1"){
+        console.log(a.innerHTML +","+n)
+         a.innerHTML = "<sup><b> <font color = 'red'>" + len +"</font></b></sup>" + a.text;
+     }
+
+
 }
